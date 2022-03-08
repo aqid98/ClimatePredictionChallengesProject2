7e2ae5b0992d14bf8e677d2f19e27c1b831b78dc
Mohammed Aqid Khatkhatay <36581744+aqid98@users.noreply.github.com>
2022-03-08 09:40:35 -0500

    Update LSTM.py

--- architectures/LSTM.py
+++ architectures/LSTM.py
@@ -1,7 +1,6 @@
 import torch.nn as nn
 import torch
 
-
 class LSTM_base(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_dim, n_layers):
         super(LSTM_base, self).__init__()
@@ -24,40 +23,9 @@ class LSTM_base(nn.Module):
         return out
     
     
-class LSTM_depth_cnn(nn.Module):
+class LSTM_depth_rnn_spatial(nn.Module):
     def __init__(self, input_dim, output_dim, hidden_dim, n_layers):
-        super(LSTM_depth_cnn, self).__init__()
-        self.output_size = output_dim
-        self.n_layers = n_layers
-        self.hidden_dim = hidden_dim
-        self.lstm = nn.LSTM(input_size = hidden_dim, 
-                            hidden_size = hidden_dim, 
-                            num_layers = n_layers, 
-                            batch_first=True)
-        
-        self.depth_cnn = nn.Conv1d(in_channels = input_dim, out_channels = hidden_dim, padding = 1, kernel_size = 3)
-        
-        self.fc = nn.Linear(hidden_dim, output_dim)
-        
-    def forward(self, x):
-        batch, depth, seq_len, feat_dim = x.shape
-        
-        x = x.permute(0, 2, 1, 3)
-        x = x.reshape(batch * seq_len, depth, feat_dim)
-        
-        depth_out = self.depth_cnn(x.permute(0, 2, 1)).transpose(1, 2)
-        depth_out = depth_out.reshape(batch, seq_len, depth, self.hidden_dim)
-        depth_out = depth_out.permute(0, 2, 1, 3)
-        
-        depth_out = depth_out.reshape(batch * depth, seq_len, self.hidden_dim)
-        lstm_out, hidden = self.lstm(depth_out)
-        out = self.fc(lstm_out)
-        out = out.reshape(batch, depth, seq_len, 1).squeeze()
-        return out
-    
-class LSTM_depth_rnn(nn.Module):
-    def __init__(self, input_dim, output_dim, hidden_dim, n_layers):
-        super(LSTM_depth_rnn, self).__init__()
+        super(LSTM_depth_rnn_spatial, self).__init__()
         self.output_size = output_dim
         self.n_layers = n_layers
         self.hidden_dim = hidden_dim
@@ -73,7 +41,7 @@ class LSTM_depth_rnn(nn.Module):
                                   batch_first = True)
         
         
-        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.fc = nn.LazyLinear(output_dim)
         
     def forward(self, x):
         batch, depth, seq_len, feat_dim = x.shape
@@ -91,6 +59,48 @@ class LSTM_depth_rnn(nn.Module):
         out = out.reshape(batch, depth, seq_len, 1).squeeze()
         return out
     
+    
+class LSTM_depth_rnn_temporal(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dim, n_layers):
+        super(LSTM_depth_rnn_temporal, self).__init__()
+        self.output_size = output_dim
+        self.n_layers = n_layers
+        self.hidden_dim = hidden_dim
+        self.lstm = nn.LSTM(input_size = input_dim, 
+                            hidden_size = hidden_dim, 
+                            num_layers = n_layers, 
+                            batch_first=True)
+        
+        self.depth_lstm = nn.LSTM(input_size = hidden_dim,
+                                  hidden_size = hidden_dim,
+                                  num_layers = n_layers,
+                                  bidirectional = True,
+                                  batch_first = True)
+        
+        
+        self.fc = nn.LazyLinear(output_dim)
+        
+    def forward(self, x):
+        batch, depth, seq_len, feat_dim = x.shape
+        
+        x = x.reshape(batch * depth, seq_len, feat_dim)
+        lstm_out, hidden = self.lstm(x)
+        lstm_out = lstm_out.reshape(batch, depth, seq_len, self.hidden_dim)
+        lstm_out = lstm_out.permute(0, 2, 1, 3)
+        
+        
+        lstm_out = lstm_out.reshape(batch * seq_len, depth, self.hidden_dim)
+        depth_out, hid_depth = self.depth_lstm(lstm_out)
+        
+        out = self.fc(depth_out)
+        out = out.reshape(batch, seq_len, depth, 1)
+        out = out.permute(0,2,1,3).squeeze()
+        
+        
+        return out
+    
+    
+    
 class LSTM_attention(nn.Module):
     def __init__(self, 
                  input_dim, 
@@ -106,60 +116,64 @@ class LSTM_attention(nn.Module):
                             hidden_size = hidden_dim, 
                             num_layers = n_layers, 
                             batch_first=True)
-        
         self.attention = nn.MultiheadAttention(embed_dim = hidden_dim, 
                                                num_heads = num_head, 
                                                batch_first = True)
         self.fc = nn.Linear(hidden_dim, output_dim)
-        
     def forward(self, x):
         batch, depth, seq_len, feat_dim = x.shape
-        
         x = x.reshape(batch * depth, seq_len, feat_dim)
         lstm_out, hidden = self.lstm(x)
-        
-        mask = torch.triu(torch.ones([seq_len, seq_len], dtype=torch.bool), diagonal=1)
+        mask = torch.triu(torch.ones([seq_len, seq_len], dtype=torch.bool), 
+                          diagonal=1).to(x.device)
         #mask = ~torch.tril(torch.ones((seq_len, seq_len), dtype=torch.bool, device=x.device))
         attn_output, attn_weights = self.attention(query = lstm_out, 
                                       key = lstm_out, 
                                       value = lstm_out,
                                       attn_mask = mask)
-        
         out = self.fc(attn_output)
         out = out.reshape(batch, depth, seq_len, 1).squeeze()
         return out
-    
-    
-    
-class LSTM_encoder(nn.Module):
-    def __init__(self, 
-                 input_dim, 
-                 output_dim,
-                 hidden_dim, 
-                 n_layers, 
-                 num_head = 1):
-        super(LSTM_encoder, self).__init__()
+class LSTM_depth_rnn_spatial_attn(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_dim, n_layers):
+        super(LSTM_depth_rnn_spatial_attn, self).__init__()
         self.output_size = output_dim
         self.n_layers = n_layers
         self.hidden_dim = hidden_dim
-        self.lstm = nn.LSTM(input_size = input_dim, 
+        self.lstm = nn.LSTM(input_size = hidden_dim * 2, 
                             hidden_size = hidden_dim, 
                             num_layers = n_layers, 
                             batch_first=True)
         
-        self.encoder = nn.TransformerEncoderLayer(d_model = hidden_dim, nhead = num_head, 
-                                                  dim_feedforward=hidden_dim * 2, batch_first = True)
-        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.depth_lstm = nn.LSTM(input_size = input_dim,
+                                  hidden_size = hidden_dim,
+                                  num_layers = n_layers,
+                                  bidirectional = True,
+                                  batch_first = True)
+        
+        self.attention = nn.MultiheadAttention(embed_dim = hidden_dim, 
+                                               num_heads = 1, 
+                                               batch_first = True)
+        self.fc = nn.LazyLinear(output_dim)
         
     def forward(self, x):
         batch, depth, seq_len, feat_dim = x.shape
         
-        x = x.reshape(batch * depth, seq_len, feat_dim)
-        lstm_out, hidden = self.lstm(x)
+        x = x.permute(0, 2, 1, 3)
+        x = x.reshape(batch * seq_len, depth, feat_dim)
         
-        mask = torch.triu(torch.ones([seq_len, seq_len], dtype=torch.bool), diagonal=1).to(x.device)
-        attn_output = self.encoder(lstm_out,
-                                      src_mask = mask)
+        depth_out, hid_depth = self.depth_lstm(x)
+        depth_out = depth_out.reshape(batch, seq_len, depth, self.hidden_dim * 2)
+        depth_out = depth_out.permute(0, 2, 1, 3)
+        
+        depth_out = depth_out.reshape(batch * depth, seq_len, self.hidden_dim * 2)
+        lstm_out, hidden = self.lstm(depth_out)
+        mask = torch.triu(torch.ones([seq_len, seq_len], dtype=torch.bool), 
+                          diagonal=1).to(x.device)
+        attn_output, attn_weights = self.attention(query = lstm_out, 
+                                      key = lstm_out, 
+                                      value = lstm_out,
+                                      attn_mask = mask)
         
         out = self.fc(attn_output)
         out = out.reshape(batch, depth, seq_len, 1).squeeze()
